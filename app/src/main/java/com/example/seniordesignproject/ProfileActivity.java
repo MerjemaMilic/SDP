77f416a9899242b4468f60c17c76e4b96a892fcb
MerjemaMilic <merjema.milic@gmail.com>
2020-05-22 20:20:31 +0200

    Profile activity updated

--- app/src/main/java/com/example/seniordesignproject/ProfileActivity.java
+++ app/src/main/java/com/example/seniordesignproject/ProfileActivity.java
@@ -4,6 +4,10 @@ import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -12,6 +16,19 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FirebaseAuth mFirebaseAuth;
+        FirebaseAuth.AuthStateListener mAuthStateListener;
+        final TextView username;
+        username = findViewById(R.id.profileusernametext);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String name = user.getEmail();
+            username.setText(name);
+            // User is signed in
+        } else {
+            // No user is signed in
+        }
 
     }
 }
